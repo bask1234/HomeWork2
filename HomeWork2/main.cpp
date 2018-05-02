7ec1876e2e6b02ca7a64658f623771a00d61b276
Matan Raviv <matanraviv@matans-imac.local>
2018-05-02 18:38:29 +0300

    Initial Commit

---
+++ HomeWork2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomeWork2
+//
+//  Created by Matan Raviv on 5/2/18.
+//  Copyright © 2018 Matan Raviv. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
